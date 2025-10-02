1e89718d07da5668f63d3d89d8923fdad0fba3a8
Bird <nigelbird99@outlook.com>
2025-10-02 16:50:52 -0500

    Bullet functionality and playerController script

--- ProjectVortex/Assets/Scripts/playerController.cs
+++ ProjectVortex/Assets/Scripts/playerController.cs
@@ -107,7 +107,7 @@ public class playerController : MonoBehaviour, IDamage
 
         if (HP <= 0)
         {
-            gamemanager.instance.youLose();
+            GameManager.instance.stateLose();
         }
     }
 
