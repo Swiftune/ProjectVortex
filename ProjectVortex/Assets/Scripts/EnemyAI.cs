89e0c05f7e60873fe6144892ce3d66c535dc03d9
MTARedHammer <arringtonmyles142@gmail.com>
2025-10-05 01:25:00 -0400

    Tried to make a patrol system

--- ProjectVortex/Assets/Scripts/EnemyAI.cs
+++ ProjectVortex/Assets/Scripts/EnemyAI.cs
@@ -7,29 +7,62 @@ public class EnemyAI : MonoBehaviour, IDamage
     [SerializeField] Renderer model;
     [SerializeField] NavMeshAgent agent;
     [SerializeField] int HP;
+    [SerializeField] float stopDist;
+    [SerializeField] float stopTime;
     [SerializeField] Transform shootPos;
     [SerializeField] GameObject bullet;
+    [SerializeField] GameObject patrolStart;
+    [SerializeField] GameObject patrolEnd;
     [SerializeField] float shootRate;
+    [SerializeField] bool withGun;
 
     Color colorOrig;
     float shootTimer;
     bool playerInRange;
+    bool pointReached;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         colorOrig = model.material.color;
+        GameManager.instance.UpdateGameGoal(1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        shootTimer += Time.deltaTime;
+        if (!withGun)
+        {
+            shootPos = null;
+            bullet = null;
+        }
+        if(Time.deltaTime % stopTime == 0)
+        {
+            pointReached = true;
+        }
+        else
+        {
+            pointReached = false;
+        }
+            shootTimer += Time.deltaTime;
         if (playerInRange)
         {
-            agent.SetDestination(gameManager.instance.player.transform.position);
+            agent.stoppingDistance = stopDist;
+            agent.SetDestination(GameManager.instance.player.transform.position);
             if (shootTimer > shootRate)
             {
                 shoot();
+            }
+        }
+        else
+        {
+            agent.stoppingDistance = 0;
+            if (pointReached)
+            {
+                agent.SetDestination(patrolEnd.transform.position);
+            }
+            else
+            {
+                agent.SetDestination(patrolStart.transform.position);
             }
         }
     }
@@ -59,6 +92,7 @@ public class EnemyAI : MonoBehaviour, IDamage
         if (HP <= 0)
         {
             Destroy(gameObject);
+            GameManager.instance.UpdateGameGoal(-1);
         }
         else
         {
