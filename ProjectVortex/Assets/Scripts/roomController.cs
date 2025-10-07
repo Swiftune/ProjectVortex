d38d6f8939674dc56932a0aef57ec1d99ba42e61
Bird <nigelbird99@outlook.com>
2025-10-07 18:22:42 -0500

    Created NavMesh prefab from Myles's Scene

--- ProjectVortex/Assets/Scripts/roomController.cs
+++ ProjectVortex/Assets/Scripts/roomController.cs
@@ -1,24 +1,55 @@
-using NUnit.Framework;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class roomController : MonoBehaviour
+public class RoomController : MonoBehaviour
 {
-
-    [SerializeField] private GameObject DoorExit;
-    [SerializeField] private GameObject DoorEnter;
+    [SerializeField] private GameObject doorExit;
+    [SerializeField] private GameObject doorEnter;
     [SerializeField] private List<GameObject> enemies;
+    [SerializeField] private GameObject triggerArea;
 
+    bool roomCleared;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // Lock both doors at the start if there are any enemies
+        if (enemies.Count > 0)
+        {
+            SetDoorsLocked(true);
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (roomCleared)
+            return;
+
+        bool allDead = true;
+
+        // Check if all enemies in the list are destroyed or disabled
+        for (int i = 0; i < enemies.Count; i++)
+        {
+            if (enemies[i] != null && enemies[i].activeInHierarchy)
+            {
+                allDead = false;
+                break;
+            }
+        }
+
+        // Unlock doors once all enemies are gone
+        if (allDead)
+        {
+            roomCleared = true;
+            SetDoorsLocked(false);
+        }
+    }
+
+    void SetDoorsLocked(bool locked)
+    {
+        if (doorEnter != null)
+            doorEnter.SetActive(locked);
+
+        if (doorExit != null)
+            doorExit.SetActive(locked);
     }
 }
