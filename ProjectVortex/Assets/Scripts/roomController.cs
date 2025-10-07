236727519bd4ca980de07146297606794880d73f
Bird <nigelbird99@outlook.com>
2025-10-07 14:50:41 -0500

    Added room and roomController

---
+++ ProjectVortex/Assets/Scripts/roomController.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class roomController : MonoBehaviour
+{
+
+    [SerializeField] private GameObject DoorExit;
+    [SerializeField] private GameObject DoorEnter;
+    [SerializeField] private List<GameObject> enemies;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
