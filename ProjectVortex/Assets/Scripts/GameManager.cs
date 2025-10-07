e3540e608d79dbc542cb0d95f1fa73aab41cb7bf
Bird <nigelbird99@outlook.com>
2025-10-06 20:12:51 -0500

    Fixed Up The Pause Menu
    Changed UI organization and fixed the issue where the pause button was GetButton instead of GetButtonDown.

--- ProjectVortex/Assets/Scripts/GameManager.cs
+++ ProjectVortex/Assets/Scripts/GameManager.cs
@@ -33,7 +33,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButton("Cancel"))
+        if (Input.GetButtonDown("Cancel"))
         {
             if (menuActive == null)
             {
