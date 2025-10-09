60fea44810415b2df4ab27ff82208bfca51ff54a
Adamg159 <Adamgflvs@gmail.com>
2025-10-08 21:24:58 -0400

    Update GameManager.cs

--- ProjectVortex/Assets/Scripts/Managers/GameManager.cs
+++ ProjectVortex/Assets/Scripts/Managers/GameManager.cs
@@ -109,4 +109,9 @@ public class GameManager : MonoBehaviour
     {
         return menuActive;
     }
+
+    public void setActiveMenu(GameObject newMenu)
+    {
+        menuActive = newMenu;
+    }
 }
