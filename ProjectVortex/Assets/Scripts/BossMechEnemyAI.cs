7cbbf1cd80b561c75011fa5079472395ec5f8537
Bird <nigelbird99@outlook.com>
2025-10-13 14:16:06 -0500

    Added dysfunctional knockback

--- ProjectVortex/Assets/Scripts/BossMechEnemyAI.cs
+++ ProjectVortex/Assets/Scripts/BossMechEnemyAI.cs
@@ -188,7 +188,7 @@ public class BossEnemyAI : MonoBehaviour, IDamage
         shootTimer3 = 0;
 
     }
-    public void takeDamage(int amount)
+    public void takeDamage(int amount, Vector3 direction)
     {
         HP -= amount;
         agent.SetDestination(GameManager.instance.player.transform.position);
