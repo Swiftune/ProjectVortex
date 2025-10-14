ed1b7b03a85b0c2f70abfc6f6643a3a2707a9902
Bird <nigelbird99@outlook.com>
2025-10-14 16:00:01 -0500

    fixed first level

--- ProjectVortex/Assets/Scripts/Managers/GameManager.cs
+++ ProjectVortex/Assets/Scripts/Managers/GameManager.cs
@@ -37,7 +37,6 @@ public class GameManager : MonoBehaviour
     private void Start()
     {
         Time.timeScale = timeScaleOrig;
-        stateMain();
     }
 
     // Update is called once per frame
