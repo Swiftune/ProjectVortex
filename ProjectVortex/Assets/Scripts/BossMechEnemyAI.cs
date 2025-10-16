dfdb9e78bd3f344abe6d65e2d4913f6bcbbc8add
MTARedHammer <arringtonmyles142@gmail.com>
2025-10-15 20:43:24 -0400

    polishing

--- ProjectVortex/Assets/Scripts/BossMechEnemyAI.cs
+++ ProjectVortex/Assets/Scripts/BossMechEnemyAI.cs
@@ -4,7 +4,7 @@ using UnityEngine.AI;
 
 public class BossEnemyAI : MonoBehaviour, IDamage
 {
-    [SerializeField] Renderer model;
+    [SerializeField] Renderer[] model;
     [SerializeField] NavMeshAgent agent;
     [SerializeField] Transform headPos;
     [SerializeField] Transform bodyRot;
@@ -25,11 +25,10 @@ public class BossEnemyAI : MonoBehaviour, IDamage
     [SerializeField] float mortarRate;
     [SerializeField] Animator animate;
 
-    Material mat;
     Color colorOrig;
-    float shootTimer1;
-    float shootTimer2;
-    float shootTimer3;
+    float cannonTimer;
+    float mgTimer;
+    float mortarTimer;
     float roamTimer;
     float angleToPlayer;
     float stoppingDistOrg;
@@ -41,7 +40,10 @@ public class BossEnemyAI : MonoBehaviour, IDamage
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        EnsureMat();
+        for (int i = 0; i < model.Length; i++)
+        {
+            colorOrig = model[i].material.color;
+        }
         stoppingDistOrg = agent.stoppingDistance;
         startingPos = transform.position;
         speedOrig = agent.speed;
@@ -51,9 +53,9 @@ public class BossEnemyAI : MonoBehaviour, IDamage
     // Update is called once per frame
     void Update()
     {
-        shootTimer1 += Time.deltaTime;
-        shootTimer2 += Time.deltaTime;
-        shootTimer3 += Time.deltaTime;
+        cannonTimer += Time.deltaTime;
+        mgTimer += Time.deltaTime;
+        mortarTimer += Time.deltaTime;
         if (agent.remainingDistance < 0.01f)
         {
             roamTimer += Time.deltaTime;
@@ -88,31 +90,6 @@ public class BossEnemyAI : MonoBehaviour, IDamage
         }
     }
 
-    bool EnsureMat()
-    {
-        // if the model is missing or is not on the scene, find on the enemy
-        if (model == null || !model.gameObject.scene.IsValid())
-        {
-            // finds the enemy render
-            model = GetComponentInChildren<Renderer>(true);
-        }
-
-        // Bail if can't find the model
-        if (model == null)
-        {
-            return false;
-        }
-
-        // set the material
-        if (mat == null)
-        {
-            mat = model.material;
-            colorOrig = mat.color;
-        }
-
-        return true;
-    }
-
     void checkRoam()
     {
         if (roamTimer >= roamPauseTime && agent.remainingDistance < 0.01f)
@@ -145,24 +122,13 @@ public class BossEnemyAI : MonoBehaviour, IDamage
                 {
                     faceTarget();
                     agent.stoppingDistance = stoppingDistOrg;
-                    if(shootTimer2 > gunRate)
-                    {
-                        fireMachineGuns();
-                        shootTimer2 = 0;
-                        shootTimer1 = cannonRate - 5;
-                    }
-                    if(shootTimer1 > cannonRate)
-                    {
-                        fireCannons();
-                        shootTimer1 = 0;
-                    }
+                    StartCoroutine(firingPattern());
                 }
                 else
                 {
-                    if (shootTimer3 > mortarRate)
+                    if (mortarTimer > mortarRate)
                     {
                         fireMortar();
-                        shootTimer3 = 0;
                     }
                 }
                 return true;
@@ -174,7 +140,7 @@ public class BossEnemyAI : MonoBehaviour, IDamage
 
     void faceTarget()
     {
-        Quaternion rot = Quaternion.LookRotation(new Vector3(playerDir.x, -playerDir.y, -playerDir.z));
+        Quaternion rot = Quaternion.LookRotation(new Vector3(playerDir.x, 0, playerDir.z));
         bodyRot.transform.rotation = Quaternion.Lerp(bodyRot.transform.rotation, rot, Time.deltaTime * faceTargetSpeed);
     }
     private void OnTriggerEnter(Collider other)
@@ -194,25 +160,33 @@ public class BossEnemyAI : MonoBehaviour, IDamage
     }
     void fireCannons()
     {
-        shootTimer1 = 0;
-        for(int i = 0; i < cannons.Length; i++)
+        Quaternion rot = Quaternion.LookRotation(new Vector3(playerDir.x, playerDir.y, playerDir.z));
+        if (cannonTimer > cannonRate)
         {
-            Instantiate(cannonRound, cannons[i].position, cannons[i].rotation);
+            cannonTimer = 0;
+            for (int i = 0; i < cannons.Length; i++)
+            {
+                Instantiate(cannonRound, cannons[i].position, rot);
+            }
         }
 
     }
     void fireMachineGuns()
     {
-        shootTimer2 = 0;
-        for (int i = 0; i < machineGuns.Length; i++)
+        Quaternion rot = Quaternion.LookRotation(new Vector3(playerDir.x, playerDir.y, playerDir.z));
+        if (mgTimer > gunRate)
         {
-            Instantiate(cannonRound, machineGuns[i].position, machineGuns[i].rotation);
+            mgTimer = 0;
+            for (int i = 0; i < machineGuns.Length; i++)
+            {
+                Instantiate(cannonRound, machineGuns[i].position, rot);
+            }
         }
 
     }
     void fireMortar()
     {
-        shootTimer3 = 0;
+        mortarTimer = 0;
         Instantiate(explosiveShell, mortar.position, mortar.rotation);
 
     }
@@ -231,15 +205,27 @@ public class BossEnemyAI : MonoBehaviour, IDamage
     }
     IEnumerator flashRed()
     {
-        model.material.color = Color.red;
-        yield return new WaitForSeconds(0.1f);
-        model.material.color = colorOrig;
+        for (int i = 0; i < model.Length; i++)
+        {
+            model[i].material.color = Color.red;
+        }
+        for (int i = 0; i < model.Length; i++)
+        {
+            yield return new WaitForSeconds(0.1f);
+            model[i].material.color = colorOrig;
+        }
     }
     IEnumerator pauseForDeath()
     {
         animate.SetTrigger("Death");
         yield return new WaitForSeconds(5f);
         Destroy(gameObject);
+    }
+    IEnumerator firingPattern()
+    {
+        fireMachineGuns();
+        yield return new WaitForSeconds(6.5f);
+        fireCannons();
     }
 
     void still()
--- ProjectVortex/Assets/Scripts/MeleeEnemyAI.cs
+++ ProjectVortex/Assets/Scripts/MeleeEnemyAI.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using UnityEngine.AI;
 public class MeleeEnemyAI : MonoBehaviour, IDamage
 {
-    [SerializeField] Renderer model;
+    [SerializeField] Renderer[] model;
     [SerializeField] NavMeshAgent agent;
     [SerializeField] Transform headPos;
     [SerializeField] int HP;
@@ -13,7 +13,6 @@ public class MeleeEnemyAI : MonoBehaviour, IDamage
     [SerializeField] float attackRate;
     [SerializeField] Animator animate;
 
-    Material mat;
     Color colorOrig;
     float attackTimer;
     float movingToPlayer;
@@ -22,7 +21,10 @@ public class MeleeEnemyAI : MonoBehaviour, IDamage
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        EnsureMat();
+        for (int i = 0; i < model.Length; i++)
+        {
+            colorOrig = model[i].material.color;
+        }
     }
 
     // Update is called once per frame
@@ -30,10 +32,7 @@ public class MeleeEnemyAI : MonoBehaviour, IDamage
     {
         attackTimer += Time.deltaTime;
         movingToPlayer += Time.deltaTime;
-        if (RushPlayer())
-        {
-
-        }
+        RushPlayer();
         if(movingToPlayer != 0)
         {
             run();
@@ -43,43 +42,16 @@ public class MeleeEnemyAI : MonoBehaviour, IDamage
             still();
         }
     }
-    bool EnsureMat()
+    void RushPlayer()
     {
-        // if the model is missing or is not on the scene, find on the enemy
-        if (model == null || !model.gameObject.scene.IsValid())
-        {
-            // finds the enemy render
-            model = GetComponentInChildren<Renderer>(true);
-        }
-
-        // Bail if can't find the model
-        if (model == null)
-        {
-            return false;
-        }
-
-        // set the material
-        if (mat == null)
-        {
-            mat = model.material;
-            colorOrig = mat.color;
-        }
-
-        return true;
-    }
-    bool RushPlayer()
-    {
+        faceTarget();
+        agent.SetDestination(GameManager.instance.player.transform.position);
         playerDir = GameManager.instance.player.transform.position - headPos.position;
         angleToPlayer = Vector3.Angle(playerDir, transform.forward);
         Debug.DrawRay(headPos.position, playerDir, Color.greenYellow);
         RaycastHit Hit;
         if (Physics.Raycast(headPos.position, playerDir, out Hit))
         {
-            if (angleToPlayer <= FOV && Hit.collider.CompareTag("Player"))
-            {
-                faceTarget();
-                agent.SetDestination(GameManager.instance.player.transform.position);
-            }
             if(agent.remainingDistance <= agent.stoppingDistance)
             {
                 movingToPlayer = 0;
@@ -88,9 +60,7 @@ public class MeleeEnemyAI : MonoBehaviour, IDamage
                     attack();
                 }
             }
-            return true;
         }
-        return false;
     }
     void faceTarget()
     {
@@ -116,9 +86,15 @@ public class MeleeEnemyAI : MonoBehaviour, IDamage
     }
     IEnumerator flashRed()
     {
-        model.material.color = Color.red;
-        yield return new WaitForSeconds(0.1f);
-        model.material.color = colorOrig;
+        for (int i = 0; i < model.Length; i++)
+        {
+            model[i].material.color = Color.red;
+        }
+        for (int i = 0; i < model.Length; i++)
+        {
+            yield return new WaitForSeconds(0.1f);
+            model[i].material.color = colorOrig;
+        }
     }
 
     void still()
